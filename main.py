41e69dbade8484b70758fe5b63c2cea0e63a2f0c
srijarkoroy <sr8962@srmist.edu.in>
2020-09-07 00:24:47 +0530

    Added input image via image address

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import streamlit as st
 st.title("App Testing")
-st.header('Working with images')
-st.subheader('Image input')
+st.header('Please enter the image for classification')
+st.subheader('Image input: ')
 
 from PIL import Image
-if st.checkbox('To upload image for clasification, check the box'):
-    if st.button("Choose an image..."):
-        file = st.file_uploader('Select', type = 'jpg')
+if st.checkbox('Upload image from device'):
+    
+    file = st.file_uploader('Select', type = ['jpg', 'png'])
 
-        st.set_option('deprecation.showfileUploaderEncoding', False)
-        if file is not None:
-            image = Image.open(file)
-            st.subheader('Displaying the uploaded image')
-            st.image(image, width = 300, caption = 'Uploaded Image')
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+    if file is not None:
+        image = Image.open(file)
+        st.subheader('Displaying the uploaded image')
+        st.image(image, width = 300, caption = 'Uploaded Image')
+
+if st.checkbox('Upload image via link'):
+    img = st.text_input('Enter Link for the image')
+    
+    if st.button('Submit'):
+        import urllib.request
+        response = Image.open(urllib.request.urlopen(img))
+        st.image(response, width = 300, caption = 'Uploaded Image')
+        
+
+
